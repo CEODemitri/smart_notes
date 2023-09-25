d20b1b5798bd3ca8077a6d6f3299020542e158c4
CEODemitri <demichole@gmail.com>
2023-09-25 15:05:43 -0500

    working on authentication

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return HttpResponse('Hello World!')
+    # return HttpResponse('Hello World!')
+    # how to render an html; use render function with 3 parameters
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
 
+
+@login_required
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
 # Create your views here.
