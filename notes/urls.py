d9068ea8de673a396d8c5a25f267e8691446b30b
CEODemitri <demichole@gmail.com>
2023-10-01 22:14:08 -0500

    adding robust front end

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
-    path('notes', views.list),
-    path('notes/<int:pk>', views.detail)
-}
+urlpatterns = [
+    path('notes', views.NotesListView.as_view(), name="notes.list"),
+    path('notes/<int:pk>', views.NotesDetailsView.as_view(), name="notes.detail")
+]
